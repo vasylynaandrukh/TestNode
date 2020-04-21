549ca9bab18ef47e14af9081d5cac2b26f35b4fc
vasylynaandrukh <vasylynavas@ukr.net>
2020-04-21 17:50:23 +0300

    add connection to db

---
+++ database/knex.js
@@ -0,0 +1,7 @@
+const enviroment = 'development'
+const config = require('../knexfile')
+const enviromentConfig = config[enviroment]
+const knex = require('knex')
+const connection = knex(enviromentConfig)
+
+module.exports = connection;
