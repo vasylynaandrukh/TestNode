1c65f110675d9823897427e7d600f7817ab46d92
vasylynaandrukh <vasylynavas@ukr.net>
2020-04-21 17:51:31 +0300

    handle req and res

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const config = require('config')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const methodOverride = require('method-override')
 
 
 const app = express()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(methodOverride());
+
 app.use('/api/auth', require('./routes/auth'))
+
+
 
 const PORT = config.get('port') || 5000
 
