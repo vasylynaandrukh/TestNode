6ca33d00b8265c7ab5c80c7cd392db3e3e2cf528
vasylynaandrukh <vasylynavas@ukr.net>
2020-04-28 08:54:12 +0300

    add open id connect

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const config = require('config')
+const configPort = require('config')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const methodOverride = require('method-override')
@@ -15,7 +15,37 @@ app.use(methodOverride());
 app.use('/api/auth', require('./routes/auth'))
 
 
+//*************
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
-const PORT = config.get('port') || 5000
+const config = {
+  required: false,
+  auth0Logout: true,
+  appSession: {
+    secret: '58eaca62128aa1cb919ad348d3e00ef71a9be0e0e6438ee65430f9d3b3c3ce0c'
+  },
+  baseURL: 'http://localhost:3000',
+  clientID: 'M0sCJ8jOUms6hYKWczHrsfZBDoKb6aw0',
+  issuerBaseURL: 'https://dev-ew45cv8o.eu.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.openid.user));
+});
+
+
+//************ */
+
+const PORT = configPort.get('port') || 3000
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
